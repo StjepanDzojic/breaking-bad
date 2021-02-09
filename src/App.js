15089c9b47f145b8c8f68201c017a8cbe603fd3e
Stjepan <dzojicsjepan1812@gmail.com>
2021-02-09 03:46:04 +0100

    Adding cards with the informations of each character

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, {useState,useEffect} from 'react'
-import './App.css';
-import Header from './components/ui/Header';
-import axios from 'axios'
-import CharacterGrid from './components/characters/CharacterGrid';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/ui/Header";
+import axios from "axios";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
-const App = () =>{
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchItems = async () =>{
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
 
-      setItems(result.data)
-      setIsLoading(false)
-    }
+      setItems(result.data);
+      setIsLoading(false);
+    };
 
-    fetchItems()
-  }, [])
+    fetchItems();
+  }, []);
+  
   return (
     <div className="container">
       <Header />
-      <CharacterGrid isLoading={isLoading} items={items}/>
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
-}
+};
 
 export default App;
